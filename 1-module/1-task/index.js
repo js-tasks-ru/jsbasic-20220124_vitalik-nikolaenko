f4d001f1a91d570375e43271cf636c4df2579b5e
VitalN <chameleon_us@mail.ru>
2022-02-02 21:13:58 +0300

    1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
-function factorial(n) {
-  // ваш код...
+﻿function factorial(n) {
+  if ((n === 0) || (n === 1)) {
+    return 1
+  } else {
+    let f = n;
+    while (n > 1) {
+      f *= --n;
+    }
+    return f
+  }
 }

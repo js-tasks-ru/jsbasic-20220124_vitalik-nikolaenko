f79baaac0c658c53769c3c9fcc13d21c12a73b80
VitalN <chameleon_us@mail.ru>
2022-02-04 18:33:20 +0300

    2-module 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
-let calculator = {
-  // ваш код
+﻿let calculator = {
+  sum() {
+    return this.a + this.b
+  },
+  mul() {
+    return this.a * this.b
+  },
+  read(a, b) {
+    this.a = a;
+    this.b = b
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ

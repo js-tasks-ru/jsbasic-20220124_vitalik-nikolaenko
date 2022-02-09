9a91bfd825b4eddf3327630e78068523a667d640
VitalN <chameleon_us@mail.ru>
2022-02-09 21:24:25 +0300

    3-module 5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,5 @@
-function getMinMax(str) {
-  // ваш код...
+﻿function getMinMax(str) {
+  let strNum = str.split(' ').map(item => +item)
+		.filter(item => !isNaN(item));
+  return {min: strNum.reduce((a, b) => a < b ? a : b), max: strNum.reduce((a, b) => a > b ? a : b)}
 }

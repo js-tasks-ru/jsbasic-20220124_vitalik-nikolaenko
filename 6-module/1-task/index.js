7a95bbf8b5b12f6551c2c2d9f83e6bbb27ca027c
VitalN <chameleon_us@mail.ru>
2022-02-26 23:33:42 +0300

    6-module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,25 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+
+    this.elem.insertAdjacentHTML('afterBegin', '<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead>');
+
+    const tBody = this.elem.createTBody();
+    
+    for (const row of rows) {
+      tBody.insertAdjacentHTML('beforeEnd', `<tr><td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td><td><button> X </button></td></tr>`);
+    }
+
+    this.elem.addEventListener('click', this.onClick);
+
+  }
+
+  onClick(event) {
+    const row = event.target.closest('TR');
+
+    if (event.target.matches('button')) {
+      row.remove();
+    }
   }
 }
